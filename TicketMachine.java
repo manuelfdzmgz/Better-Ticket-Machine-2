614a4b417d45ca59debe37e2c381c1a221f66c3e
unknown <mmanuelfdzmgz@gmail.com>
2014-11-09 23:18:10 +0100

    añadido metodo que vacia la maquina y devuelve el dinero que habia

--- TicketMachine.java
+++ TicketMachine.java
@@ -1,5 +1,4 @@
-/**
- * TicketMachine models a ticket machine that issues
+/* TicketMachine models a ticket machine that issues
  * flat-fare tickets.
  * The price of a ticket is specified via the constructor.
  * Instances will check to ensure that a user only enters
@@ -108,4 +107,14 @@ public class TicketMachine
         balance = 0;
         return amountToRefund;
     }
+    public int emptyMachine()
+    {
+        int emptyMachine;
+        emptyMachine = total;
+        total = 0;
+        return emptyMachine;
+        
+        
+    }
+   
 }
